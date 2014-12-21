8e74478dfab8a256f5c3b56b3f0ec2a226a68520
Bibek Shrestha <bibekshrestha@gmail.com>
2014-12-21 11:50:15 +0100

    Removed unnecessary files and fixed routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221074138) do
+ActiveRecord::Schema.define(version: 20141221104132) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "note"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.text     "title"
+    t.boolean  "finished"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
