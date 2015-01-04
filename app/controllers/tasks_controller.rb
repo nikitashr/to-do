57d33c31b92659dd14ed46e97a93795134eae671
Nikita <nikitashr@gmail.com>
2015-01-04 12:23:01 +0545

    app folder updated

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,6 +30,6 @@ class TasksController < ApplicationController
 
   private
     def task_params
-      params.require(:task).permit(:title, :note)
+      params.require(:task).permit(:title)
     end
 end
