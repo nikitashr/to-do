34548f6fb7cfa9617a02c43a25706fd6c005b9f9
Nikita <nikitashr@gmail.com>
2015-10-01 11:27:13 +0545

    minor updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Todo.create!(title: 'grocery shopping', notes: 'pickles, eggs, red onion')
